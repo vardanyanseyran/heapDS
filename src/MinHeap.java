5aad6f9bf130099502a6e49bf0223a6b0d62b4a5
macbook <servard1002@gmail.com>
2024-02-19 10:49:05 +0400

    Some implementation of Heap Data Structure

---
+++ src/MinHeap.java
@@ -0,0 +1,115 @@
+import java.util.Arrays;
+
+public class MinHeap {
+    private int[] heap;
+    private int size;
+    private int capacity;
+
+    public MinHeap(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        this.heap = new int[capacity];
+    }
+
+    private int getParentIndex(int childIndex) {
+        return (childIndex - 1) / 2;
+    }
+
+    private int getLeftChildIndex(int parentIndex) {
+        return 2 * parentIndex + 1;
+    }
+
+    private int getRightChildIndex(int parentIndex) {
+        return 2 * parentIndex + 2;
+    }
+
+    private boolean hasParent(int index) {
+        return getParentIndex(index) >= 0;
+    }
+
+    private boolean hasLeftChild(int index) {
+        return getLeftChildIndex(index) < size;
+    }
+
+    private boolean hasRightChild(int index) {
+        return getRightChildIndex(index) < size;
+    }
+
+    private int parent(int index) {
+        return heap[getParentIndex(index)];
+    }
+
+    private int leftChild(int index) {
+        return heap[getLeftChildIndex(index)];
+    }
+
+    private int rightChild(int index) {
+        return heap[getRightChildIndex(index)];
+    }
+
+    private void swap(int index1, int index2) {
+        int temp = heap[index1];
+        heap[index1] = heap[index2];
+        heap[index2] = temp;
+    }
+
+    public void insert(int value) {
+        if (size == capacity) {
+            heap = Arrays.copyOf(heap, capacity * 2);
+            capacity *= 2;
+        }
+        heap[size] = value;
+        size++;
+        pullUp();
+    }
+
+    private void pullUp() {
+        int index = size - 1;
+        while (hasParent(index) && parent(index) > heap[index]) {
+            swap(getParentIndex(index), index);
+            index = getParentIndex(index);
+        }
+    }
+
+    private void pullDown() {
+        int index = 0;
+        while (hasLeftChild(index)) {
+            int smallerChildIndex = getLeftChildIndex(index);
+            if (hasRightChild(index) && rightChild(index) < leftChild(index)) {
+                smallerChildIndex = getRightChildIndex(index);
+            }
+
+            if (heap[index] < heap[smallerChildIndex]) {
+                break;
+            } else {
+                swap(index, smallerChildIndex);
+            }
+
+            index = smallerChildIndex;
+        }
+    }
+
+    public int extractMin() {
+        if (size == 0) {
+            throw new IllegalStateException("Heap is empty");
+        }
+        int minValue = heap[0];
+        heap[0] = heap[size - 1];
+        size--;
+        pullDown();
+
+        return minValue;
+    }
+
+    public static void main(String[] args) {
+        MinHeap minHeap = new MinHeap(10);
+
+        minHeap.insert(4);
+        minHeap.insert(8);
+        minHeap.insert(2);
+        minHeap.insert(7);
+        minHeap.insert(1);
+
+        System.out.println("Extracted Min --- " + minHeap.extractMin());
+    }
+}
